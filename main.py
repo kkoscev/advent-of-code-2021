ddf54e923adb0378e95f03e933aa77471ee7d96d
kkoscevic <karlo.koscevic@fer.hr>
2021-12-16 09:52:12 +0100

    Implement Day 3 solution

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def read(filepath, map_fn):
     with open(filepath, 'r') as f:
         return list(map(map_fn, f.readlines()))
@@ -35,8 +38,67 @@ def day2(course):
     print(f'Final => horizontal position: {horizontal_pos}, depth: {depth} (product: {horizontal_pos * depth})')
 
 
+def day3part1(report):
+    gamma_rate = ''
+    epsilon_rate = ''
+    for bit_idx in range(len(report[0].strip())):
+        n_zeros = 0
+        n_ones = 0
+
+        for line in report:
+            if line.strip()[bit_idx] == '0':
+                n_zeros += 1
+            else:
+                n_ones += 1
+
+        if n_zeros < n_ones:
+            gamma_rate += '1'
+            epsilon_rate += '0'
+        else:
+            gamma_rate += '0'
+            epsilon_rate += '1'
+
+    print(
+        f'Gamma rate: {gamma_rate} = {int(gamma_rate, 2)}, epsilon rate: {epsilon_rate} = {int(epsilon_rate, 2)}, '
+        f'power consumption: {int(gamma_rate, 2) * int(epsilon_rate, 2)}')
+
+
+def day3part2(report):
+    def get_rating(report, bit_idx, criteria, line_len):
+        if len(report) == 1:
+            return int(report[0], 2)
+
+        zero_bit = np.zeros(0, dtype=np.bool)
+
+        for line in report:
+            zero_bit = np.append(zero_bit, True) if line.strip()[bit_idx] == '0' else np.append(zero_bit, False)
+
+        report = report[zero_bit] if criteria(zero_bit) else report[np.invert(zero_bit)]
+        bit_idx = bit_idx + 1 if bit_idx < line_len - 1 else 0
+
+        return get_rating(report, bit_idx, criteria, line_len)
+
+    def oxygen_criteria(zero_bit):
+        return np.sum(zero_bit) > np.sum(np.invert(zero_bit))
+
+    def co2_criteria(zero_bit):
+        return np.sum(zero_bit) <= np.sum(np.invert(zero_bit))
+
+    report = np.array(report)
+    line_len = len(report[0].strip())
+    oxygen_generator_rating = get_rating(report, 0, oxygen_criteria, line_len)
+    co2_scrubber_rating = get_rating(report, 0, co2_criteria, line_len)
+
+    print(
+        f'Oxygen generator rating: {oxygen_generator_rating}, CO2 Scrubber rating: {co2_scrubber_rating} => Life '
+        f'support rating: {oxygen_generator_rating * co2_scrubber_rating}')
+
+
 if __name__ == '__main__':
-    day1(read('samples/day1', map_fn=lambda x: int(x)))
-    day1(read('puzzles/day1', map_fn=lambda x: int(x)))
-    day2(read('samples/day2', map_fn=lambda x: x))
-    day2(read('puzzles/day2', map_fn=lambda x: x))
+    # day1(read('samples/day1', map_fn=lambda x: int(x)))
+    # day1(read('puzzles/day1', map_fn=lambda x: int(x)))
+    # day2(read('samples/day2', map_fn=lambda x: x))
+    # day2(read('puzzles/day2', map_fn=lambda x: x))
+    # day3(read('samples/day3', map_fn=lambda x: x))
+    day3part1(read('puzzles/day3', map_fn=lambda x: x))
+    day3part2(read('puzzles/day3', map_fn=lambda x: x))  # Oxygen: 825, Co2: 3375 => Life support rating: 2784375
