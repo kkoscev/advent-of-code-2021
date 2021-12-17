dd9df6a1a7baaf294e44b713b8459eb7f53e2a5c
kkoscevic <karlo.koscevic@fer.hr>
2021-12-17 14:28:41 +0100

    Implement Day 4 solution

--- main.py
+++ main.py
@@ -94,11 +94,87 @@ def day3part2(report):
         f'support rating: {oxygen_generator_rating * co2_scrubber_rating}')
 
 
+def day4part1(bingo):
+    def parse_board(board):
+        return np.array(list(map(lambda x: np.fromstring(x.strip(), dtype=int, sep=' '), board)))[..., None]
+
+    boards = np.empty((5, 5, 0), dtype=int)
+    for i in range(2, len(bingo) - 4, 6):
+        boards = np.append(boards, parse_board(bingo[i:i + 5]), axis=-1)
+
+    winning_board = -1
+    last_number = -1
+
+    for number in list(map(int, bingo[0].strip().split(','))):
+        boards[boards == number] = -1
+
+        if np.any(np.sum(boards, axis=0) == -5):
+            winning_board = np.where(np.sum(boards, axis=0) == -5)[1][0]
+            last_number = number
+            break
+        elif np.any(np.sum(boards, axis=1) == -5):
+            winning_board = np.where(np.sum(boards, axis=1) == -5)[1][0]
+            last_number = number
+            break
+
+    if winning_board > -1:
+        board = boards[..., winning_board]
+        score = np.sum(board[board != -1]) * last_number
+        print(f'Winning board: {winning_board} Last number: {last_number} Unmarked sum: {np.sum(board[board != -1])} '
+              f'Score: {score}')
+
+
+def day4part2(bingo):
+    def parse_board(board):
+        return np.array(list(map(lambda x: np.fromstring(x.strip(), dtype=int, sep=' '), board)))[..., None]
+
+    boards = np.empty((5, 5, 0), dtype=int)
+    for i in range(2, len(bingo) - 4, 6):
+        boards = np.append(boards, parse_board(bingo[i:i + 5]), axis=-1)
+
+    last_winning_board = -1
+    last_number = -1
+
+    print(len(bingo[0].strip()))
+    i = 0
+    for number in list(map(int, bingo[0].strip().split(','))):
+        boards[boards == number] = -1
+        i += 1
+
+        if np.any(np.sum(boards, axis=0) == -5):
+            print(np.where(np.sum(boards, axis=0) == -5))
+            last_winning_board = np.where(np.sum(boards, axis=0) == -5)[1]
+            last_number = number
+            if boards.shape[-1] == 1:
+                break
+            else:
+                boards = np.delete(boards, last_winning_board, axis=-1)
+        elif np.any(np.sum(boards, axis=1) == -5):
+            print(np.where(np.sum(boards, axis=1) == -5))
+            last_winning_board = np.where(np.sum(boards, axis=1) == -5)[1]
+            last_number = number
+            if boards.shape[-1] == 1:
+                break
+            else:
+                boards = np.delete(boards, last_winning_board, axis=-1)
+
+    if last_winning_board > -1:
+        board = boards[..., last_winning_board]
+        score = np.sum(board[board != -1]) * last_number
+        print(
+            f'Winning board: {last_winning_board} Last number: {last_number} Unmarked sum: '
+            f'{np.sum(board[board != -1])} '
+            f'Score: {score}')
+
+
 if __name__ == '__main__':
     # day1(read('samples/day1', map_fn=lambda x: int(x)))
     # day1(read('puzzles/day1', map_fn=lambda x: int(x)))
     # day2(read('samples/day2', map_fn=lambda x: x))
     # day2(read('puzzles/day2', map_fn=lambda x: x))
     # day3(read('samples/day3', map_fn=lambda x: x))
-    day3part1(read('puzzles/day3', map_fn=lambda x: x))
-    day3part2(read('puzzles/day3', map_fn=lambda x: x))  # Oxygen: 825, Co2: 3375 => Life support rating: 2784375
+    # day3part1(read('puzzles/day3', map_fn=lambda x: x))
+    # day3part2(read('puzzles/day3', map_fn=lambda x: x))  # Oxygen: 825, Co2: 3375 => Life support rating: 2784375
+    # day4part1(read('samples/day4', map_fn=lambda x: x))
+    day4part1(read('puzzles/day4', map_fn=lambda x: x))  # Board: 41 Last num: 48 Unmarked sum: 932 Score: 44736
+    day4part2(read('puzzles/day4', map_fn=lambda x: x))  # Board: 0 Last number: 7 Unmarked sum: 261 Score: 1827
